e6c8ff5075e02257317287116ca9c4c6d5625967
Andy <85425901+Andy17937@users.noreply.github.com>
2024-05-26 18:03:07 +0800

    Controller

---
+++ Gadgets/Gadgets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gadgets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
